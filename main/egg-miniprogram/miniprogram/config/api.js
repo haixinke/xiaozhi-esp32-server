dbd906244d425f5e685f96e3c5c5217b58405a3a
minwang <min.wang@igen-tech.com>
2026-07-16 21:39:48 +0800

    chore(egg-miniprogram): switch api base to production and update demo submodule

--- main/egg-miniprogram/miniprogram/config/api.js
+++ main/egg-miniprogram/miniprogram/config/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = 'http://192.168.48.81:8002/xiaozhi';
+// const API_BASE_URL = 'http://192.168.1.27:8002/xiaozhi';
 
-// const API_BASE_URL = 'https://api.eggbabe.com/xiaozhi';
+const API_BASE_URL = 'https://api.eggbabe.com/xiaozhi';
 
 module.exports = { API_BASE_URL };
